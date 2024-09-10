ed32ca4dfa7e823057741e3201cf61984ac04bc2
Sumeet Khillare <shreddersk@shreddersk-mac.local>
2024-09-10 12:40:27 -0400

    Task1: Used static analysis tools and fixed the code.(autopep8, pylint, pyflakes).

--- hw2/hw2_debugging.py
+++ hw2/hw2_debugging.py
@@ -1,36 +1,52 @@
+"""
+hw2_debugging.py
+
+This module contains code for sorting list, and uses rand.py to generate random numbers.
+Date: 2024-09-10
+"""
 import rand
 
-def mergeSort(arr):
-    if (len(arr) == 1):
+def merge_sort(arr):
+    """
+    Merge sort code
+    Return:
+        Sorted list
+    """
+    if len(arr) == 1:
         return arr
 
     half = len(arr)//2
 
-    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+    return recombine(merge_sort(arr[:half]), merge_sort(arr[half:]))
 
-def recombine(leftArr, rightArr):
-    leftIndex = 0
-    rightIndex = 0
-    mergeArr = [None] * (len(leftArr) + len(rightArr))
-    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
-        if leftArr[leftIndex] < rightArr[rightIndex]:
-            rightIndex += 1
-            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
+
+def recombine(left_arr, right_arr):
+    """
+    Combine subproblems
+    Return:
+        Sorted list
+    """
+    left_index = 0
+    right_index = 0
+    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    while left_index < len(left_arr) and right_index < len(right_arr):
+        if left_arr[left_index] < right_arr[right_index]:
+            right_index += 1
+            merge_arr[left_index + right_index] = left_arr[left_index]
         else:
-            leftIndex += 1
-            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
+            left_index += 1
+            merge_arr[left_index + right_index] = right_arr[right_index]
 
-    for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[i]
-    
-    for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
+    for i in range(right_index, len(right_arr)):
+        merge_arr[left_index + right_index] = right_arr[i]
 
-    return mergeArr
+    for i in range(left_index, len(left_arr)):
+        merge_arr[left_index + right_index] = left_arr[i]
 
-arr = rand.random_array([None] * 20)
-arr_out = mergeSort(arr)
+    return merge_arr
+
+
+random_arr = rand.random_array([None] * 20)
+arr_out = merge_sort(random_arr)
 
 print(arr_out)
-
-
--- hw2/rand.py
+++ hw2/rand.py
@@ -1,8 +1,22 @@
+"""
+rand.py
+
+This module contains random_array function, which generates random numbers from 1 to 20.
+Date: 2024-09-10
+"""
 import subprocess
 
+
 def random_array(arr):
+    """
+    Generates a random integer between 1 and 20.
+
+    Returns:
+        arr: list of random integers.
+    """
     shuffled_num = None
     for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
+        shuffled_num = subprocess.run(
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
