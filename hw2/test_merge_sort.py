c6a0d4f9cc6ff244a1c00a2d0367fcd1d08ce083
xpincsu <xpi@ncsu.edu>
2024-09-10 15:32:49 -0400

    Update test_merge_sort.py

--- hw2/test_merge_sort.py
+++ hw2/test_merge_sort.py
@@ -1,17 +1,17 @@
 #sorting an empty array, a single element array, multiple elements, duplicates, and a reverse-ordered array. 
-from hw2_debugging import mergeSort
+from hw2_debugging import merge_sort
 
 def test_sort_empty():
-    assert mergeSort([]) == []
+    assert merge_sort([]) == []
 
 def test_sort_single_element():
-    assert mergeSort([1]) == [1]
+    assert merge_sort([1]) == [1]
 
 def test_sort_multiple_elements():
-    assert mergeSort([4, 2, 7, 1, 9]) == [1, 2, 4, 7, 9]
+    assert merge_sort([4, 2, 7, 1, 9]) == [1, 2, 4, 7, 9]
 
 def test_sort_duplicates():
-    assert mergeSort([4, 2, 4, 1, 2]) == [1, 2, 2, 4, 4]
+    assert merge_sort([4, 2, 4, 1, 2]) == [1, 2, 2, 4, 4]
 
 def test_sort_reverse_order():
-    assert mergeSort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
+    assert merge_sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
