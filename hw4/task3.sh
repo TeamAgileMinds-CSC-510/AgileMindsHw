17f25d7e40a0a52e214c216364443ec23189b972
shreddersk <sumeetkhillare10@gmail.com>
2024-10-10 16:14:10 -0400

    modified task3

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-awk -F',' 'NR>1 && $3==2 && $13~"S"' titanic.csv | sed 's/female/F/g; s/male/M/g' | awk -F',' '{sum += $7; count++} END {if (count > 0) print sum / count}'
+result=$(awk -F',' 'NR>1 && $3==2 && $13~"S"' titanic.csv | sed 's/female/F/g; s/male/M/g')
+
+echo "$result"
+
+echo "$result" | awk -F',' '{sum += $7; count++} END {if (count > 0) print "Average Age: " sum / count}'
+
